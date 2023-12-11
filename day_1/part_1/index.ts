66676cb515492f46a0bc6f97ab8004bd1ba2aebe
hchang604 <henry.chang@alianza.com>
2023-12-10 19:06:44 -0800

    separate day 1 part 1 directly

--- day_1/part_1/index.ts
+++ day_1/part_1/index.ts
@@ -5,15 +5,16 @@ const FILE_PATH = './day_1/part_1/calibration_values.txt'
 /* Read values from files */
 fs.readFile(FILE_PATH, 'utf8', (err, data) => {
   if (err) {
-    console.log(`Error reading file: ${err}`)
+    console.log(
+      `Error reading file: ${err}\nCheck if your file is formatted correctly.`,
+    )
     return
   }
 
   /*
    * Split values into array by \r\n
-   * Remove last element of array because it is undefined
    */
-  const array = data.split('\r\n').slice(0, -1)
+  const array = data.split('\r\n')
 
   /* Extract digits from each value */
   const calibrationValues = array.map((value) => getDigits(value))
